6052a5e139fbb5207edd302b105c5213da89aaef
paulav88 <paula.sv88@gmail.com>
2018-05-09 00:20:21 -0300

    Main Fitlandia (Rutinas - Running - Selfie)

---
+++ app/src/main/java/ar/com/fitlandia/fitlandia/MainActivity.java
@@ -0,0 +1,32 @@
+package ar.com.fitlandia.fitlandia;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button Rutinas;
+    Button Running;
+    Button Selfie;
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
+
+     }
+
+    public void goToRutinas(View v){
+        Intent intent = new Intent(this, Rutinas.class);
+        startActivity(intent);
+    }
+    public void goToRunning(View v){
+        Intent intent = new Intent(this, Running.class);
+        startActivity(intent);
+    }
+    public void goToSelfie(View v){
+        Intent intent = new Intent(this, Selfie.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/ar/com/fitlandia/fitlandia/Rutinas.java
@@ -0,0 +1,13 @@
+package ar.com.fitlandia.fitlandia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Rutinas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rutinas);
+    }
+}
