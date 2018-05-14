181d21b3c804e70bab0c2015129f7c0d7e4a9cd3
mely <camilamel_18@hotmail.com>
2018-05-13 22:47:54 -0300

    "Refact"

--- app/src/main/java/ar/com/fitlandia/fitlandia/MainActivity.java
+++ app/src/main/java/ar/com/fitlandia/fitlandia/MainActivity.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -78,7 +75,7 @@ public class MainActivity extends AppCompatActivity
 
 
 
-     }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
