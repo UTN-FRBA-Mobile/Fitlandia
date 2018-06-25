dc2756f3db18ae44c73afdbe91db049499982786
paulav88 <paula.sv88@gmail.com>
2018-06-25 00:46:58 -0300

    Perfil de usuario toma 2

--- app/src/main/java/ar/com/fitlandia/fitlandia/Perfil.java
+++ app/src/main/java/ar/com/fitlandia/fitlandia/Perfil.java
@@ -1,13 +1,37 @@
 package ar.com.fitlandia.fitlandia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ar.com.fitlandia.fitlandia.models.UsuarioModel;
+import butterknife.ButterKnife;
+
+import butterknife.BindView;
 
 public class Perfil extends AppCompatActivity {
+    @BindView(R.id.edad) EditText edad;
+    @BindView(R.id.EditTextName) EditText nombre;
+    @BindView(R.id.altura) EditText altura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+    }
+    public void actualizar(View v){
+        ButterKnife.bind(this);
+        Toast toast1 =  Toast.makeText(getApplicationContext(), "Cargando tu perfil..."+nombre.getText(), Toast.LENGTH_SHORT);
+        toast1.show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
     }
 }
